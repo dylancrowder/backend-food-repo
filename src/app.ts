5b88203e55ee47cceda32cf3de23e0b9b14c5a8c
dylan rojo <devdylancrowder@outlook.com>
2024-07-22 21:31:58 -0300

    strict

--- src/app.ts
+++ src/app.ts
@@ -51,8 +51,8 @@ app.use((req: any, res, next) => {
       httpOnly: true,
       secure: true,
       maxAge: 30 * 24 * 60 * 60 * 1000,
-      sameSite: "none",
-      domain: "https://backendfood.vercel.app",
+      sameSite:"strict"
+    
     });
 
     return res.json({ message: token });
